0c362d2671a0ca0b7dce6d06a8e539855e961f27
bkuste2 <igorribeiro632@gmail.com>
2024-09-30 04:32:35 -0300

    🚀 feat: add relation with albums in user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
+  has_many :albums
+
+  accepts_nested_attributes_for :albums
+
   enum role: { user: 'user', admin: 'admin' }
 
   validates :full_name, presence: true
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, on: :create
   validates :role, inclusion: { in: roles.keys }
-
   validate :username_must_be_alphanumeric
 
   private
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :role, :full_name, :username, :password, :created_at, :updated_at
+  attributes :id, :role, :full_name, :username, :password, :albums, :created_at, :updated_at
 end
